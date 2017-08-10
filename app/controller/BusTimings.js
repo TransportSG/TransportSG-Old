95cc6fd36dd99d2b720d8e37c9f30515da1e97b4
Eyeball Edward <edwardyeung.private@gmail.com>
2017-08-10 20:48:21 +0800

    Fixed issue with SWT trips

--- app/controller/BusTimings.js
+++ app/controller/BusTimings.js
@@ -36,7 +36,7 @@ function createOffset(timings, ageMillis) {
 function getTerminalForService(busService, givenDestination) {
 	return new Promise(function(resolve, reject) {
 		BusService.findOne({
-			fullService: busService.replace(/[WG]/g, '')
+			fullService: busService.replace(/[A-Z]/g, '')
 		}, (err, service) => {
 			if (err) throw err;
 			var stops = service.stops;
